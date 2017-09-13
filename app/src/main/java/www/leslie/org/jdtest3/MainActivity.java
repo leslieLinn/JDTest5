c22721fd266af6d00e354877a4fb399a5e200d59
leslie <leslie_linjing@163.com>
2017-09-13 16:55:35 +0800

    第一次部分提交，添加了Button变量

--- app/src/main/java/www/leslie/org/jdtest3/MainActivity.java
+++ app/src/main/java/www/leslie/org/jdtest3/MainActivity.java
@@ -2,11 +2,13 @@ package www.leslie.org.jdtest3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView v;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
