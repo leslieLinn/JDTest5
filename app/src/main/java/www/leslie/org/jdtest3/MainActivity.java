45e2ad7b2e56faba30459bb48bb5d561caa2c181
leslie <leslie_linjing@163.com>
2017-09-13 17:01:13 +0800

    第三次部分提交，添加了Person类

--- app/src/main/java/www/leslie/org/jdtest3/MainActivity.java
+++ app/src/main/java/www/leslie/org/jdtest3/MainActivity.java
@@ -3,12 +3,15 @@ package www.leslie.org.jdtest3;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView v;
     Button btn;
+    EditText mEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/www/leslie/org/jdtest3/Person.java
@@ -0,0 +1,8 @@
+package www.leslie.org.jdtest3;
+
+/**
+ * Created by Administrator on 2017/9/13.
+ */
+
+public class Person {
+}
